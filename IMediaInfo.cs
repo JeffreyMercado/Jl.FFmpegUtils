a29b4b42b266b151710ad8e89615bc850093c969
Jeff Mercado <JeffreyMercado@users.noreply.github.com>
2022-06-19 06:01:32 -0700

    added more properties

--- IMediaInfo.cs
+++ IMediaInfo.cs
@@ -3,6 +3,9 @@ namespace Jl.FFmpegUtils;
 public interface IMediaInfo
 {
     IMediaSource Source { get; }
+    TimeSpan Duration { get; }
+    long Size { get; }
+    DateTime? CreationTime { get; }
     IReadOnlyList<IMediaStream> Streams { get; }
     IReadOnlyList<IVideoStream> VideoStreams { get; }
     IReadOnlyList<IAudioStream> AudioStreams { get; }
