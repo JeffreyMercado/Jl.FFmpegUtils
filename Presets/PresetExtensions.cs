0f9e4c6847d13fddd39f572558ddc3eb047887af
Jeff Mercado <JeffreyMercado@users.noreply.github.com>
2022-07-06 21:16:04 -0700

    making presets configurable

--- Presets/PresetExtensions.cs
+++ Presets/PresetExtensions.cs
@@ -2,33 +2,47 @@
 
 public static class PresetExtensions
 {
-    public static IFFmpegOutputBuilder FastStandardOutputFormat(this IFFmpegOutputBuilder builder) => builder
-        .AddVideoStream(v => v
-            .CodecLibx264(x => x
+    public static IFFmpegOutputBuilder FastStandardOutputFormat(
+        this IFFmpegOutputBuilder builder,
+        Action<IFFmpegOutputVideoStreamBuilder>? configureVideo = default,
+        Action<IFFmpegOutputAudioStreamBuilder>? configureAudio = default
+    ) =>
+        builder.AddVideoStream(v =>
+        {
+            v.CodecLibx264(x => x
                 .Profile("main")
                 .Level("4.0")
                 .Preset(Libx264.Libx264Preset.fast)
                 .Crf(22)
             )
-            .FpsMax(30)
-        )
-        .AddAudioStream(a => a
-            .Codec(AudioCodec.aac)
-            .Bitrate("160k")
-        );
+            .FpsMax(30);
+            configureVideo?.Invoke(v);
+        })
+        .AddAudioStream(a =>
+        {
+            a.Codec(AudioCodec.aac).Bitrate("160k");
+            configureAudio?.Invoke(a);
+        });
 
-    public static IFFmpegOutputBuilder HQStandardOutputFormat(this IFFmpegOutputBuilder builder) => builder
-        .AddVideoStream(v => v
-            .CodecLibx264(x => x
+    public static IFFmpegOutputBuilder HQStandardOutputFormat(
+        this IFFmpegOutputBuilder builder,
+        Action<IFFmpegOutputVideoStreamBuilder>? configureVideo = default,
+        Action<IFFmpegOutputAudioStreamBuilder>? configureAudio = default
+    ) =>
+        builder.AddVideoStream(v =>
+        {
+            v.CodecLibx264(x => x
                 .Profile("high")
                 .Level("4.0")
                 .Preset(Libx264.Libx264Preset.slow)
                 .Crf(20)
             )
-            .FpsMax(30)
-        )
-        .AddAudioStream(a => a
-            .Codec(AudioCodec.aac)
-            .Bitrate("160k")
-        );
+            .FpsMax(30);
+            configureVideo?.Invoke(v);
+        })
+        .AddAudioStream(a =>
+        {
+            a.Codec(AudioCodec.aac).Bitrate("160k");
+            configureAudio?.Invoke(a);
+        });
 }
