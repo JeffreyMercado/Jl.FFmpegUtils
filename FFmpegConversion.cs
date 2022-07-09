076705804a33a708841b0648bdd3ae48ee7440a8
Jeff Mercado <JeffreyMercado@users.noreply.github.com>
2022-07-09 01:46:05 -0700

    have conversion process output more useful info

--- FFmpegConversion.cs
+++ FFmpegConversion.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text;
 
 namespace Jl.FFmpegUtils;
 
@@ -10,8 +11,11 @@ public record FFmpegConversion(IFFmpegClArguments Arguments) : IFFmpegConversion
     public async Task<IFFmpegConversionResult> ConvertAsync(IFFmpegProvider provider, CancellationToken cancellationToken = default)
     {
         var arguments = Arguments.SerializeArguments();
-        var observer = new DataObserver(this);
-        var result = await provider.ConvertAsync(arguments, observer, cancellationToken).ConfigureAwait(false);
+        var observer = new DataObserver(this, DateTime.Now, cancellationToken);
+        var observerTask = observer.Task;
+        var convertTask = provider.ConvertAsync(arguments, observer, cancellationToken);
+        await Task.WhenAll(observerTask, convertTask).ConfigureAwait(false);
+        var result = convertTask.Result;
         return new FFmpegConversionResult(Arguments, result.StartTime, result.EndTime);
     }
     private void OnDataReceived(ConversionData e)
@@ -23,21 +27,29 @@ public record FFmpegConversion(IFFmpegClArguments Arguments) : IFFmpegConversion
         ProgressReceived?.Invoke(this, e);
     }
 
-    private record FFmpegConversionResult(IFFmpegClArguments Arguments, DateTime StartTime, DateTime EndTime) : IFFmpegConversionResult;
+    private record FFmpegConversionResult(IFFmpegClArguments Arguments, DateTime StartTime, DateTime EndTime) : IFFmpegConversionResult
+    {
+        public IFFmpegOutputSink Output => Arguments.Output.Sink;
+        public TimeSpan Duration => EndTime - StartTime;
+    }
 
     private class DataObserver : IObserver<DataReceivedEventArgs>
     {
         private readonly TaskCompletionSource tcs = new TaskCompletionSource();
+        private readonly StringBuilder buffer = new StringBuilder();
         private readonly FFmpegConversion conversion;
+        private readonly DateTime startTime;
         private readonly long? size;
         private readonly TimeSpan? duration;
-        public DataObserver(FFmpegConversion conversion)
+        public DataObserver(FFmpegConversion conversion, DateTime startTime, CancellationToken cancellationToken)
         {
             this.conversion = conversion;
+            this.startTime = startTime;
             var inputs = conversion.Arguments.Inputs;
             (duration, size) = inputs.Count == 1 && inputs.Single() is IFFmpegInput input
                 ? (input.Duration, input.Size)
                 : default;
+            cancellationToken.Register(() => tcs.TrySetCanceled(cancellationToken));
         }
 
         public Task Task => tcs.Task;
@@ -47,17 +59,32 @@ public record FFmpegConversion(IFFmpegClArguments Arguments) : IFFmpegConversion
             if (value.Data is string data)
             {
                 if (ConversionProgress.TryParse(data, size, duration) is ConversionProgress progress)
+                {
                     conversion.OnProgressReceived(progress);
+                }
                 else
+                {
+                    buffer.AppendLine(data);
                     conversion.OnDataReceived(new ConversionData(data));
+                }
             }
         }
 
         public void OnError(Exception error)
         {
-            // suppress cancellations here
+            // suppress cancellations here, let main thread handle that
             if (error is not OperationCanceledException)
-                tcs.TrySetException(error);
+            {
+                // FFmpeg errors aren't clearly reported, output the prefiltered buffer for the message
+                var exception = new FFmpegConversionException(
+                    "Conversion failed",
+                    buffer.ToString(),
+                    conversion.Arguments,
+                    startTime,
+                    DateTime.Now
+                );
+                tcs.TrySetException(exception);
+            }
         }
 
         public void OnCompleted()
@@ -65,4 +92,16 @@ public record FFmpegConversion(IFFmpegClArguments Arguments) : IFFmpegConversion
             tcs.TrySetResult();
         }
     }
+}
+
+public class FFmpegConversionException : Exception, IFFmpegConversionResult
+{
+    public FFmpegConversionException(string message, string outputBuffer, IFFmpegClArguments arguments, DateTime startTime, DateTime endTime)
+        : base(message) => (OutputBuffer, Arguments, StartTime, EndTime) = (outputBuffer, arguments, startTime, endTime);
+    public string OutputBuffer { get; }
+    public IFFmpegClArguments Arguments { get; }
+    public IFFmpegOutputSink Output => Arguments.Output.Sink;
+    public DateTime StartTime { get; }
+    public DateTime EndTime { get; }
+    public TimeSpan Duration => EndTime - StartTime;
 }
--- interfaces/IFFmpegConversionResult.cs
+++ interfaces/IFFmpegConversionResult.cs
@@ -3,6 +3,7 @@ namespace Jl.FFmpegUtils
     public interface IFFmpegConversionResult
     {
         IFFmpegClArguments Arguments { get; }
+        IFFmpegOutputSink Output => Arguments.Output.Sink;
         DateTime StartTime { get; }
         DateTime EndTime { get; }
         TimeSpan Duration => EndTime - StartTime;
