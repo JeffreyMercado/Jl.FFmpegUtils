e06339b96a571b142731cc639baf371e0062afb6
Jeff Mercado <JeffreyMercado@users.noreply.github.com>
2022-07-30 03:04:30 -0700

    add configure for global builder

--- extensions/ClArgumentsBuilderExtensions.cs
+++ extensions/ClArgumentsBuilderExtensions.cs
@@ -4,6 +4,11 @@ using Arguments;
 
 public static class ClArgumentsBuilderExtensions
 {
+    public static IFFmpegClArgumentsBuilder Configure(this IFFmpegClArgumentsBuilder builder, Func<IFFmpegClArgumentsBuilder, IFFmpegClArgumentsBuilder>? config)
+    {
+        return config?.Invoke(builder) ?? builder;
+    }
+
     /// <summary>-y</summary>
     public static IFFmpegClArgumentsBuilder Overwrite(this IFFmpegClArgumentsBuilder builder) => builder.AddGlobal(new GlobalOverwriteArgument());
 
